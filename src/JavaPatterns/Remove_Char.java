d091ca3ab8c1c3a6d66ee162e3320f2c5a54ea2c
Kishbose <kaameshbose2002@gmail.com>
2022-12-14 12:47:48 +0530

    Java commit

---
+++ src/JavaPatterns/Remove_Char.java
@@ -0,0 +1,12 @@
+package JavaPatterns;
+
+public class Remove_Char {
+	
+	public static void main(String[] args) {
+		
+		String input="heloo038r03rjsfjo";
+		System.out.println(input);
+		System.out.println(input.replaceAll("[^0-9]",""));
+	}
+
+}
